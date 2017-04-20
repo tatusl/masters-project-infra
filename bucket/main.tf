147f7a22a454b921c0b51229db02901e55d29fe3
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2017-04-20 23:35:44 +0300

    [bucket] Change module name

--- bucket/main.tf
+++ bucket/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module "prod_bucket" {
+module "state_bucket" {
   source      = "git@github.com:tatusl/masters-project-terraform-modules.git//state_bucket"
   bucket_name = "mp-remote-state"
 }

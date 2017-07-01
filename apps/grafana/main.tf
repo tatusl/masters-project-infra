543f69263cbdb4d275fc4dcb6166a38ecb7fa124
Tatu Seppä-Lassila <tatu.seppa-lassila@weave.fi>
2017-07-01 18:20:16 +0300

    [grafana] Use file interpolation for env vars

--- apps/grafana/main.tf
+++ apps/grafana/main.tf
@@ -54,25 +54,25 @@ module "application" {
   health_check_target = "/login"
   container_port      = 3000
   ecs_cluster         = "${data.terraform_remote_state.ecs.ecs_cluster}"
-  env_vars            = "${var.env_vars}"
+  env_vars            = "${file("files/env_vars.json")}"
 }
 
-module "rds" {
-  source                    = "git@github.com:tatusl/masters-project-terraform-modules.git//rds"
-  name                      = "${var.name}-${var.env}"
-  env                       = "${var.env}"
-  engine                    = "postgres"
-  engine_version            = "9.6.2"
-  publicly_accessible       = "false"
-  database_name             = "${var.name}"
-  username                  = "${var.username}"
-  password                  = "${var.password}"
-  subnets                   = "${data.terraform_remote_state.vpc.private_subnet_ids}"
-  zone_id                   = "${data.terraform_remote_state.dns.hosted_zone_id}"
-  vpc_id                    = "${data.terraform_remote_state.vpc.vpc_id}"
-  allowed_security_group_id = "${data.terraform_remote_state.ecs.ecs_security_group}"
-  bastion_ip                = "${data.terraform_remote_state.bastion.bastion_private_ip}"
-}
+#module "rds" {
+#  source                    = "git@github.com:tatusl/masters-project-terraform-modules.git//rds"
+#  name                      = "${var.name}-${var.env}"
+#  env                       = "${var.env}"
+#  engine                    = "postgres"
+#  engine_version            = "9.6.2"
+#  publicly_accessible       = "false"
+#  database_name             = "${var.name}"
+#  username                  = "${var.username}"
+#  password                  = "${var.password}"
+#  subnets                   = "${data.terraform_remote_state.vpc.private_subnet_ids}"
+#  zone_id                   = "${data.terraform_remote_state.dns.hosted_zone_id}"
+#  vpc_id                    = "${data.terraform_remote_state.vpc.vpc_id}"
+#  allowed_security_group_id = "${data.terraform_remote_state.ecs.ecs_security_group}"
+#  bastion_ip                = "${data.terraform_remote_state.bastion.bastion_private_ip}"
+#}
 
 resource "aws_route53_record" "app" {
   zone_id = "${data.terraform_remote_state.dns.hosted_zone_id}"
